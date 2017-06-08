6f7b527032d981c2987790531741447e17c9b385
zhulinpinyu <mulixiang199016@gmail.com>
2017-06-08 10:15:00 +0800

    elasticsearch geoserver

--- geoserver/2.9/setenv.sh
+++ geoserver/2.9/setenv.sh
@@ -1,3 +1,4 @@
-export JAVA_OPTS="-XX:PermSize=512m -XX:MaxPermSize=1024m"
-#JAVA_OPTS="-Xms512m -Xmx2048m -XX:MetaspaceSize=512m -XX:MaxMetaspaceSize=1024m"
-#export JAVA_OPTS="-Xms512m -Xmx2048m -XX:PermSize=128m -XX:MaxPermSize=1024m"
+#export JAVA_OPTS="-XX:PermSize=512m -XX:MaxPermSize=1024m"
+#JAVA_OPTS="-Xms512m -Xmx2048m -XX:MetaspaceSize=512m -XX:MaxMetaspaceSize=1024m" export
+#JAVA_OPTS="-Xms1024m -Xmx4096m -XX:SoftRefLRUPolicyMSPerMB=36000 -XX:+UseParallelGC -XX:+UseParNewGC -XX:+UseG1GC"
+JAVA_OPTS="-Xms1024m -Xmx6144m -XX:SoftRefLRUPolicyMSPerMB=36000 -XX:+UseG1GC -XX:+UseStringDeduplication"
